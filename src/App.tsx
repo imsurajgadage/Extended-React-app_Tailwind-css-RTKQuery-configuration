20858e4b46dfd87b60c8e4f1783469b7d1b37889
imsurajgadage <surajgadage20@gmail.com>
2023-05-15 21:39:58 +0530

    final configuration setup completed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="">
+      <h1>Extended Scafolding application React js Tailwind CSS RTK Query</h1>
+    </div>
+  );
+}
+
+export default App;
